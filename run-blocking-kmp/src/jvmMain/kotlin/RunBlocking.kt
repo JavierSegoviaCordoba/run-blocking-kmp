f0a3b2c2780797f927add1d6e3390d52e9aa533f
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-01-30 13:31:39 +0100

    Add docs to `runBlocking` function

--- run-blocking-kmp/src/jvmMain/kotlin/RunBlocking.kt
+++ run-blocking-kmp/src/jvmMain/kotlin/RunBlocking.kt
@@ -4,6 +4,7 @@ import kotlin.coroutines.CoroutineContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
+/** JVM `runBLocking` implementation */
 public actual fun <T> runBlocking(
     context: CoroutineContext,
     block: suspend CoroutineScope.() -> T
