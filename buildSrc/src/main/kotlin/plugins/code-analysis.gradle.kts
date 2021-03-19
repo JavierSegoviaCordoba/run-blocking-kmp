3a9fd704aa30a04600d5e82d925a0ee1edfb9d42
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-03-19 23:59:26 +0100

    Improve GitHub Actions workflows

--- buildSrc/src/main/kotlin/plugins/code-analysis.gradle.kts
+++ buildSrc/src/main/kotlin/plugins/code-analysis.gradle.kts
@@ -14,6 +14,7 @@ detekt {
     isIgnoreFailures = true
     autoCorrect = false
     buildUponDefaultConfig = true
+    basePath = rootProject.projectDir.path
 }
 
 tasks.withType<Detekt>().configureEach {
