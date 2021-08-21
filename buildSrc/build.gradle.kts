4ee320ea98ec96c71c12bc0396c277c01ba65005
GitHub Action <action@github.com>
2021-08-21 00:00:59 +0000

    Bump Build Version Catalogs version to 0.1.0-alpha.48

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,21 +16,5 @@ dependencies {
     //  https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
     implementation(files(pluginLibs.javaClass.superclass.protectionDomain.codeSource.location))
 
-    pluginLibs.apply {
-        implementation(javiersc.gradlePlugins.allProjects)
-        implementation(javiersc.gradlePlugins.buildVersionCatalogsUpdater)
-        implementation(javiersc.gradlePlugins.changelog)
-        implementation(javiersc.gradlePlugins.codeAnalysis)
-        implementation(javiersc.gradlePlugins.codeFormatter)
-        implementation(javiersc.gradlePlugins.dependencyUpdates)
-        implementation(javiersc.gradlePlugins.docs)
-        implementation(javiersc.gradlePlugins.gradleWrapperUpdater)
-        implementation(javiersc.gradlePlugins.kotlinMultiplatform)
-        implementation(javiersc.gradlePlugins.nexus)
-        implementation(javiersc.gradlePlugins.pluginAccessors)
-        implementation(javiersc.gradlePlugins.publishKotlinMultiplatform)
-        implementation(javiersc.gradlePlugins.readmeBadges)
-        implementation(javiersc.gradlePlugins.versioning)
-        implementation(jetbrains.kotlinx.binaryCompatibilityValidator)
-    }
+    implementation(pluginLibs.bundles.javierscPlugins)
 }
