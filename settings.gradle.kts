8ad8af531c084c2f0cb8aae46d55ce918738115b
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2021-12-23 23:50:25 +0100

    Add `gradle-enterprise` config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
+plugins {
+    `gradle-enterprise`
+}
 
 rootProject.name = providers.gradleProperty("project.name").forUseAtConfigurationTime().get()
 
@@ -14,6 +17,13 @@ dependencyResolutionManagement {
             create("libs") { from(files("gradle/libs.toml")) }
             create("pluginLibs") { from(files("gradle/pluginLibs.toml")) }
         }
+    }
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
     }
 }
 
